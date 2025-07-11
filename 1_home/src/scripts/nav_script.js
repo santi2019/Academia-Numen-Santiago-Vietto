fa97ea19433530987cb2e070bdb929b15783dd9e
santi2019 <santiagovietto5@hotmail.com>
2025-07-10 21:04:32 -0300

    subo primera parte del proyecto home

---
+++ 1_home/src/scripts/nav_script.js
@@ -0,0 +1,19 @@
+/* Codigo que permite que la barra de navegacion cambie de color. */
+
+// Conectarme con la etiqueta que tiene la clase nav-container
+var navbar = document.querySelector.Selector('.nav_container')
+
+// Cuando haga scroll
+window.onscroll = () => {
+
+    // Si se hizo scroll pasando los 20 pixeles desde el top de la pantalla
+    if(window.scrollY > 20){
+
+        // Se le agrega a la etiqueta "navbar", con la que nos conectamos, la clase nav-active
+        navbar.classList.add('nav_active');
+    } else{
+
+        // Caso contrario, si estamos a menos de 20 pixeles, se quita la clase nav-active
+        navbar.classList.remove('nav_active');
+    }
+}
